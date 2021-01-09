64e5a5755ffde2d6d64e2ac7576831b9da0b9917
Javier Alvarez Rodriguez <javier.alvarez.practicas@telefonica.com>
2021-01-09 17:51:01 +0100

    Create main.py

---
+++ compute_vision/main.py
@@ -0,0 +1,166 @@
+import numpy as np  #Version 1.19.4
+import cv2 as cv    #Version opencv-python 3.4.2.17
+
+def get_points(img, calib):
+    rows=7
+    columns=7
+    #convertimos a escala de grises para facilitar la deteccion de las esquinas
+    gray = cv.cvtColor(img,
+                       cv.COLOR_BGR2GRAY)
+    # Buscamos las esquinas del tablero
+    ret, corners = cv.findChessboardCorners(gray,
+                                            (rows, columns),
+                                            None)
+
+    # Comprobamos si ha encontrado un tablero, en cuyo caso continiamos. Si no, devolvemos solo la imagen
+    if ret == True:
+        #Calcula la diferencia entre los puntos detectados de los elementos para dibujar los cuadrados más adelante
+        x1,y1,w,h=cv.boundingRect(corners[0])
+        x2,y2,w,h=cv.boundingRect(corners[8])
+        if(x1<x2):
+            witdth=x2-x1
+        else:
+            witdth=x1-x2
+        if(y1<y2):
+            high=y2-y1
+        else:
+            high=y1-y2
+
+        #Extraemos las coordenadas x y de los puntos y los almacenamos en un array
+        corn=[]
+        for c in corners:
+            x,y,w,h=cv.boundingRect(c)
+            corn.append([[x,y]])
+
+        #Calculamos las coordenadas para los puntos restantes
+            #Primero calculamos los de la parte de la izquierda al grupo detectado
+        for i in range(7):
+            x3=int(corn[7*i+i][0][0]) - witdth
+            y3=int(corn[7*i+i][0][1])
+            corn.insert(i*7+i,[[x3,y3]])
+
+            #Ahora a por los de la derecha
+        for i in range(7):
+            x3=int(corn[7*(i+1)+2*i][0][0]) + witdth
+            y3=int(corn[7*(i+1)+2*i][0][1])
+            corn.insert(7*(i+1)+2*i+1,[[x3,y3]])
+
+            #Ahora a por los de arriba
+        for i in range(9):
+            x3=int(corn[i*2][0][0])
+            y3=int(corn[i*2][0][1])-high
+            corn.insert(i,[[x3,y3]])
+
+            # Ahora a por los de abajo
+        for i in range(9):
+            x3 = int(corn[9 * 7 + i][0][0])
+            y3 = int(corn[9 * 7 + i][0][1]) + high
+            corn.insert(9 * 8 + i, [[x3, y3]])
+
+
+        #Dibuja encima rectangulos o devuelve el array
+        if(calib):
+            for r in range(8):
+                for c in range(8):
+                    x = int(corn[9*r+c][0][0])
+                    y = int(corn[9*r+c][0][1])
+                    cv.rectangle(img,
+                                 (x, y),
+                                 (x + witdth, y + high),
+                                 (36, 255, 12),
+                                 2)
+            return True,img;
+        else:
+            return True, corn;
+    return False,img;
+
+def resizeimg(original, scale_percent):
+    #first parameter is the image, and the second the porcentage to resize the image
+    print('Original Dimensions : ', original.shape)
+    width = int(original.shape[1] * scale_percent / 100)
+    height = int(original.shape[0] * scale_percent / 100)
+    dim = (width, height)
+    # resize image
+    img = cv.resize(original, dim, interpolation=cv.INTER_AREA)
+    return img
+
+def showimage(img,waits):
+    #show image and depends of the value of "waits", wait a key or not
+    cv.imshow("imagen",img)
+    if waits:
+        cv.waitKey(0)
+
+def checker_detection(arrays,img):
+    checkboard = np.zeros((8, 8))
+    print(checkboard, checkboard.shape)
+    #Vamos a dividir la imagen en casillas
+        #para ello primero sacamos el tamaño de los lados
+    x1=arrays[0][0][0]
+    y1=arrays[0][0][1]
+    x2 = arrays[10][0][0]
+    y2 = arrays[10][0][1]
+    if (x1 < x2):
+        witdth = x2 - x1
+    else:
+        witdth = x1 - x2
+    if (y1 < y2):
+        high = y2 - y1
+    else:
+        high = y1 - y2
+    #Ahora vamos tomando los cuadrados de la imagen de 1 en 1
+    for r in range(8):
+        for c in range(8):
+            x = int(arrays[9 * r + c][0][0])
+            y = int(arrays[9 * r + c][0][1])
+            crop_img = img[y:y + high, x:x + witdth]
+            showimage(crop_img,True)
+    # cv.waitKey(0)
+    # cv.imshow('img', img)
+    #    cv.waitKey(0)
+    return False, checkboard;
+
+
+def opencv_management(option):
+    #Esta funcion devuelve un booleano referenciando a si ha detectado el tablero, y otro valor(imagen o array)
+    #dependiendo de la opcion seleccionada
+    #Esta funcion tiene las siguientes opciones
+        # 1 -Detecta el tablero en tiempo real y la representa sobre el tablero hasta que se pulsa una tecla(se usa para calibrar)
+        # 2 -Devuelve un array con las posiciones de las esquinas (tablero una vez calibrado)
+        # 3 -Devuelve un array con la disposicion de las fichas en el tablero
+    #en cualquier caso tomaremos la imagen de la webcam
+    cap = cv.VideoCapture(0)
+    if(option==1):
+        print("PRESIONA q para salir de esta vista")
+        while (True):
+            ret, frame = cap.read()
+            detected,image = get_points(frame, True)
+            showimage(image, False)
+            if cv.waitKey(1) & 0xFF == ord('q'):
+                if (detected): print("Tablero detectado")
+                break
+        cap.release()
+        cv.destroyAllWindows()
+
+    elif(option==2):
+        ret, frame = cap.read()
+        detected, arrays = get_points(frame, False)
+        if(detected): print("Tablero detectado")
+        return detected, arrays;
+
+    elif(option==3):
+        ret, frame = cap.read()
+        detected, arrays = get_points(frame, False)
+        detected=True
+        if (detected):
+            print("Tablero detectado")
+            errors, arrays = checker_detection(arrays,frame)
+        return False,  arrays;
+    else:
+        print("tienes que elegir una opcion valida")
+
+
+def main():
+    opencv_management(3)
+
+
+main()
